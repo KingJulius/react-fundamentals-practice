14a2c875db3c86e5014b4b4b628190359f6c0be1
Julius Dsouza <jsdsz1996@gmail.com>
2024-06-20 01:48:29 -0700

    Added UseImperativeHandle Component

--- app/_types/types.ts
+++ app/_types/types.ts
@@ -1,6 +1,15 @@
+// UseReducer Component
+
 export type reducerState = {
   count: number;
   showText: boolean;
 };
 
 export type reducerAction = { type: "INCREMENT" } | { type: "TOGGLE" };
+
+
+// UseImperativeHandle Component
+
+export type ButtonHandle = {
+    alterToggle: () => void
+}
